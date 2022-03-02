780ad837c310d37e35d1c51dc50953e9968a4e81
Samuel Kraut <krauts.tin21@student.dhbw-heidenheim.de>
2022-03-02 12:57:39 +0100

    update weathermap

---
+++ javascript/mapdata.js
@@ -0,0 +1,226 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+    //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+
+    //Zoom settings
+    zoom: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "no",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    DEU1572: {
+      name: "Nordrhein-Westfalen",
+      description: "7°C"
+    },
+    DEU1573: {
+      name: "Baden-Württemberg",
+      description: "7°C"
+    },
+    DEU1574: {
+      name: "Hessen",
+      description: "7°C"
+    },
+    DEU1575: {
+      name: "Bremen",
+      description: "7°C"
+    },
+    DEU1576: {
+      name: "Niedersachsen",
+      description: "7°C"
+    },
+    DEU1577: {
+      name: "Thüringen",
+      description: "7°C"
+    },
+    DEU1578: {
+      name: "Hamburg",
+      description: "7°C"
+    },
+    DEU1579: {
+      name: "Schleswig-Holstein",
+      description: "7°C"
+    },
+    DEU1580: {
+      name: "Rheinland-Pfalz",
+      description: "7°C"
+    },
+    DEU1581: {
+      name: "Saarland",
+      description: "7°C"
+    },
+    DEU1591: {
+      name: "Bayern",
+      description: "7°C"
+    },
+    DEU1599: {
+      name: "Berlin",
+      description: "7°C"
+    },
+    DEU1600: {
+      name: "Sachsen-Anhalt",
+      description: "7°C"
+    },
+    DEU1601: {
+      name: "Sachsen",
+      description: "7°C"
+    },
+    DEU3487: {
+      name: "Brandenburg",
+      description: "7°C"
+    },
+    DEU3488: {
+      name: "Mecklenburg-Vorpommern",
+      description: "7°C"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "52.516667",
+      lng: "13.4",
+      name: "Berlin",
+      type: "circle",
+      size: "30",
+      description: "7°C"
+    },
+    "1": {
+      lat: "54.323334",
+      lng: "10.139444",
+      name: "Kiel",
+      description: "7°C",
+      size: "30",
+      type: "circle"
+    },
+    "2": {
+      lat: "54.083336",
+      lng: "12.108811",
+      name: "Rostock",
+      description: "7°C",
+      size: "30",
+      type: "circle"
+    },
+    "3": {
+      lat: "52.373920",
+      lng: "9.735603",
+      name: "Hannover",
+      description: "7°C",
+      size: "30",
+      type: "circle"
+    },
+    "4": {
+      lat: "53.551086",
+      lng: "9.993682",
+      name: "Hamburg",
+      description: "7°C",
+      size: "30",
+      type: "circle"
+    },
+    "5": {
+      lat: "50.935173",
+      lng: "6.953101",
+      name: "Köln",
+      description: "7°C",
+      size: "30",
+      type: "circle"
+    },
+    "6": {
+      lat: "50.110924",
+      lng: "8.682127",
+      name: "Frankfurt",
+      description: "7°C",
+      size: "30",
+      type: "circle"
+    },
+    "7": {
+      lat: "51.050407",
+      lng: "13.737262",
+      name: "Dresden",
+      description: "7°C",
+      size: "30",
+      type: "circle"
+    },
+    "8": {
+      lat: "48.1351253",
+      lng: "11.5819806",
+      name: "München",
+      type: "circle",
+      size: "30",
+      description: "7°C"
+    },
+    "9": {
+      lat: "48.783333",
+      lng: "9.183333",
+      name: "Stuttgart",
+      description: "7°C",
+      size: "30",
+      type: "circle"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
