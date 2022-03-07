081c89da70d1a91239c588a98346265785632591
Samuel Kraut <krauts.tin21@student.dhbw-heidenheim.de>
2022-03-07 19:38:04 +0100

    add javascript to weathermap

--- javascript/api.js
+++ javascript/api.js
@@ -7,3 +7,15 @@ async function getHistoryData(lat, lon, date) {
         }
     }).then((response) => response.json());
 }
+
+
+async function getStateData() {
+    return await fetch("data/coordinatesGermanStates.json")
+    .then((response) => response.json());
+}
+
+async function getWeatherState(lat, lon) {
+    const dateToday =  new Date().toISOString().slice(0, 10);
+    return fetch("https://api.weatherapi.com/v1/history.json?key=a8a6faad8a9d4a00a22174858220303&q="+lat+","+lon+"&dt="+dateToday+"&lang=de")
+    .then(response => response.json())
+}
