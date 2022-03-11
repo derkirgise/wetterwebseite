cb083b445e6e7f18907f9e233a3edf12d4da6e66
derkirgise <philipp.erath@gmail.com>
2022-03-11 08:51:11 +0100

    fixed https

--- javascript/api.js
+++ javascript/api.js
@@ -1,5 +1,5 @@
 async function getHistoryData(lat, lon, date) {
-    return await fetch(`http://api.weatherapi.com/v1/history.json?key=a8a6faad8a9d4a00a22174858220303&dt=${date}&lang=de&q=${lat},${lon}`, {
+    return await fetch(`https://api.weatherapi.com/v1/history.json?key=a8a6faad8a9d4a00a22174858220303&dt=${date}&lang=de&q=${lat},${lon}`, {
         mode: 'cors',
         headers: {
             'Access-Control-Allow-Origin': '*'
@@ -8,7 +8,7 @@ async function getHistoryData(lat, lon, date) {
 }
 
 async function getRealTimeData(lat, lon) {
-    return await fetch(`http://api.weatherapi.com/v1/current.json?key=a8a6faad8a9d4a00a22174858220303&lang=de&q=${lat},${lon}`, {
+    return await fetch(`https://api.weatherapi.com/v1/current.json?key=a8a6faad8a9d4a00a22174858220303&lang=de&q=${lat},${lon}`, {
         mode: 'cors',
         headers: {
             'Access-Control-Allow-Origin': '*'
@@ -17,7 +17,7 @@ async function getRealTimeData(lat, lon) {
 }
 
 async function getForecastData(lat, lon) {
-    return await fetch(`http://api.weatherapi.com/v1/forecast.json?key=a8a6faad8a9d4a00a22174858220303&lang=de&q=${lat},${lon}&days=3`, {
+    return await fetch(`https://api.weatherapi.com/v1/forecast.json?key=a8a6faad8a9d4a00a22174858220303&lang=de&q=${lat},${lon}&days=3`, {
         mode: 'cors',
         headers: {
             'Access-Control-Allow-Origin': '*'
